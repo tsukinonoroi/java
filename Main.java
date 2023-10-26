f6891f2fd79168cd9271f4240a3c7f3feb0f7800
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-10-26 18:58:31 +0300

    find pair of elem

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-// Найти такую пару элементов в массиве, чья сумма даст значение переменной target
+// Найти такую пару элементов в массиве, чья сумма даст значение переменной target //////////
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9};
@@ -7,11 +7,10 @@ public class Main {
         for (int start = 0; start < arr.length; start++) {
             for (int i = 0; i < arr.length; i++) {
                 if (arr[start] + arr[i] == target) {
-                    System.out.println("indexes of array " + start + "," + i);
+                    System.out.println("Indexes of array " + start + "," + i);
                 }
 
                 }
             }
         }
-
     }
