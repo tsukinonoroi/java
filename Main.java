9be2592d1f8a8e4b09c7dd8b0c89bcf009dc4779
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-11-01 17:50:20 +0300

    is palindrome ?

--- Main.java
+++ Main.java
@@ -1,43 +1,22 @@
 import java.util.Scanner;
-import java.util.Arrays;
-// Создание динамического двумерного массива, поиск максимального и минимального элемента в нем
+
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int rows = scan.nextInt();
-        int columns = scan.nextInt();
+        String string = scan.nextLine();
+        System.out.println(is_palindrome(string));
+    }
 
-        int[][] matrix = new int[columns][rows];
-
-        for (int i = 0; i < columns; i++) {
-            for (int j = 0; j < rows; j++) {
-                int value = scan.nextInt();
-                matrix[i][j] = value;
-            }
+    public static boolean is_palindrome(String string) {
+        StringBuilder newstr = new StringBuilder(string);
+        newstr.reverse();
+        boolean a = true, b = false;
+        if (string.equals(newstr.toString())) {
+            return a;
         }
-
-        for (int i = 0; i < columns; i++) {
-            for (int j = 0; j < rows; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
+        else {
+            return b;
         }
-
-        int min = matrix[0][0];
-        int max = matrix[0][1];
-
-        for (int i = 0; i < columns; i++) {
-            for (int j = 0; j < rows; j++) {
-                if (matrix[i][j] < min) {
-                    min = matrix[i][j];
-                }
-                else if (matrix[i][j] > max || matrix[i][j] > min) {
-                    max = matrix[i][j];
-                }
-            }
-        }
-
-        System.out.println("Max el = " + max);
-        System.out.println("Min el = " + min);
     }
 }
+
