987499e7549bc72fdcd384b5cabafb5704cc3da9
tsukinonoroi <seytumerov.e.i21@gmail.com>
2024-05-04 16:35:32 +0300

    task 12

--- Main.java
+++ Main.java
@@ -1,37 +1,38 @@
 package org.example;
-import java.math.*;
+
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
-import static java.lang.Math.pow;
-
-/*У числа 24 восемь делителей: 1, 2, 3, 4, 6, 8, 12 и 24.
-Существует десять чисел не больше 100, имеющих ровно восемь делителей:
-24, 30, 40, 42, 54, 56, 66, 70, 78 и 88.
-Пусть f(n) будет количеством чисел не больше n с ровно восемью делителями.
-Известно, что f(100) = 10, f(1000) = 180 и f(10^6) = 224427.
-Найдите f(10^9). */
 public class Main {
-    public static final int myNum = 1000000;
     public static void main(String[] args) {
-        int countNums = 0;
-        int count = 0;
-        for (int i = 1; i <= myNum; i++) {
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    count += 1;
-                }
+        ArrayList<String> arr = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+        while (arr.size() <= 2) {
+            String str = scan.next();
+            arr.add(str);
+        }
+        System.out.println(longestPrefix(arr));
 
-            }
-        if (count != 8) {
-            count = 0;
+    }
+
+
+    public static StringBuilder longestPrefix(ArrayList<String> arr) {
+    int i = 0;
+    StringBuilder stringBuilder = new StringBuilder("");
+    while(true) {
+
+        if (arr.get(0).substring(i, i + 1).equals(arr.get(1).substring(i, i + 1))
+                &&
+                arr.get(0).substring(i, i + 1).equals(arr.get(1).substring(i, i + 1)))
+        {
+            stringBuilder.append(arr.get(0).substring(i, i + 1));
+            i++;
         }
         else {
-            countNums += 1;
-            count = 0;
+            return stringBuilder;
         }
-        }
-        System.out.println(countNums);
     }
+    }
+
 }
