9bd44fba4d3d78206cf39547eff477519f44eec7
tsukinonoroi <seytumerov.e.i21@gmail.com>
2024-05-22 13:03:21 +0300

    leetcode 21

--- Main.java
+++ Main.java
@@ -1,38 +1,26 @@
 package org.example;
 
-
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> arr = new ArrayList<>();
-        Scanner scan = new Scanner(System.in);
-        while (arr.size() <= 2) {
-            String str = scan.next();
-            arr.add(str);
-        }
-        System.out.println(longestPrefix(arr));
-
+     List<Integer> list1 = new ArrayList<>();
+     List<Integer> list2 = new ArrayList<>();
+     Scanner scan = new Scanner(System.in);
+     while (list1.size() < 3) {
+         list1.add(scan.nextInt());
+     }
+        System.out.println("list 1 ---> : " + list1);
+     while (list2.size() < 3) {
+         list2.add(scan.nextInt());
+     }
+        System.out.println("list 2 ---> : " + list2);
+        List<Integer> merge = new ArrayList<>();
+        list1.addAll(list2);
+        Collections.sort(list1);
+        System.out.println("merged & sorted lists ---> " + list1);
     }
-
-
-    public static StringBuilder longestPrefix(ArrayList<String> arr) {
-    int i = 0;
-    StringBuilder stringBuilder = new StringBuilder("");
-    while(true) {
-
-        if (arr.get(0).substring(i, i + 1).equals(arr.get(1).substring(i, i + 1))
-                &&
-                arr.get(0).substring(i, i + 1).equals(arr.get(1).substring(i, i + 1)))
-        {
-            stringBuilder.append(arr.get(0).substring(i, i + 1));
-            i++;
-        }
-        else {
-            return stringBuilder;
-        }
-    }
-    }
-
 }
