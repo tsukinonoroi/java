97abb00b0e0d535b27f20204878b9101dcc49107
tsukinonoroi <seytumerov.e.i21@gmail.com>
2024-10-27 18:52:42 +0300

    leetcode 283

--- Main.java
+++ Main.java
@@ -1,41 +1,41 @@
 package org.example;
 
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
-public class Main  {
+public class Main {
 
-    public static final int size = 100_000;
-    public static int arr[] = new int[size];
     public static void main(String[] args) {
-        Random r = new Random();
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        List<Integer> arr = new ArrayList<>();
+
+        addElementToArr(arr, scan, size);
+
+        System.out.println(arr);
+
+        zeroToEnd(arr);
+
+        System.out.println(arr);
+
+    }
+
+    public static void addElementToArr(List<Integer> arr, Scanner scan, int size) {
         for (int i = 0; i < size; i++) {
-            arr[i] = r.nextInt(500_000);
+            arr.add(scan.nextInt());
         }
-
-        findMax(size, arr);
-
     }
 
-    public static void findMax(int size, int arr[]) {
-        maxThread thread1 = new maxThread(0, size/2, arr);
-        maxThread thread2 = new maxThread(size/2, size, arr);
-
-        thread1.start();
-        thread2.start();
-
-        try {
-            thread1.join();
-            thread2.join();
+    public static void zeroToEnd(List<Integer> arr) {
+        Collections.sort(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) == 0) {
+                arr.addLast(arr.get(i));
+                arr.remove(arr.get(i));
+            }
         }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        int max1 = thread1.getMax();
-        int max2 = thread2.getMax();
-        System.out.println(Math.max(max1, max2));
     }
-
 }
-
