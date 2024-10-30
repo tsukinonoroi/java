bb518585186eb09f270b6e223c47e46d15bdba60
tsukinonoroi <seytumerov.e.i21@gmail.com>
2024-10-30 21:41:10 +0300

    leetcode 977

--- Main.java
+++ Main.java
@@ -1,38 +1,32 @@
 package org.example;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
-        isPowerOfFour(num);
-
-    }
-
-    public static void isPowerOfFour(int num) {
-        for (int i = 0; i < 100_000; i++) {
-            if (pow(4, i) == num) {
-                System.out.println("true" + " " + i);
-                break;
-            }
-            else if(pow(4, i) > num) {
-                System.out.println("false" + " " + i); //степень, которая превышает заданный юзером num ; юзер задал 325 (превышет 256(4 степень)), но не дотягивает до 1024(5 степень)
-                break;
-            }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+          list.add(scan.nextInt());
         }
+
+        squareNums(list);
+        Collections.sort(list);
+        System.out.println(list);
     }
 
-    public static int pow(int number, int i) {
-        int result = 1;
-        for (int j = 0; j < i; j++) {
-            result *= number;
+    public static List<Integer> squareNums(List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            int j = list.get(i);
+            j*=j;
+            list.set(i, j);
         }
-        return result;
+        return list;
     }
+
+
 }
